ec5489d35a19dfdff5d11283daa2bac9e18762dd
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-09-09 10:21:10 -0700

    Typography adjustments to line heights

--- src/App.js
+++ src/App.js
@@ -7,19 +7,69 @@ function App() {
     <div className='App container'>
       <h1>Stardust</h1>
 
-      <div className='component'>
+      <div className='section'>
         <h2>Branding</h2>
-        <Branding />
+        <div className='component'>
+          <Branding />
+        </div>
       </div>
 
-      <div className='component'>
+      <div className='section'>
         <h2>Profile</h2>
-        <Profile
-          user={{
-            name: 'Ben de Jesus',
-            picture: 'http://placekitten.com/200/200'
-          }}
-        />
+        <div className='component'>
+          <Profile
+            user={{
+              name: 'Ben de Jesus',
+              picture: 'http://placekitten.com/200/200'
+            }}
+          />
+        </div>
+      </div>
+
+      <div className='section typography'>
+        <h2>Typography</h2>
+
+        <div className='grid'>
+          <div className='component'>
+            <h3>h1</h3>
+            <h1>
+              Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.
+            </h1>
+          </div>
+
+          <div className='component'>
+            <h3>h2</h3>
+            <h2>
+              Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.
+            </h2>
+          </div>
+
+          <div className='component'>
+            <h3>h3</h3>
+            <h3>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h3>
+          </div>
+
+          <div className='component'>
+            <h3>h4</h3>
+            <h4>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h4>
+          </div>
+
+          <div className='component'>
+            <h3>h5</h3>
+            <h5>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h5>
+          </div>
+
+          <div className='component'>
+            <h3>h6</h3>
+            <h6>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h6>
+          </div>
+
+          <div className='component'>
+            <h3>Paragraph</h3>
+            <p>Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum.</p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
