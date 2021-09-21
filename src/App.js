0beecf9ea498ff389af4491e77c0aab820c592db
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-09-21 14:54:42 -0700

    System message styles

--- src/App.js
+++ src/App.js
@@ -37,51 +37,103 @@ function App() {
 
         <div className='grid'>
           <div className='component'>
-            <h3>h1</h3>
+            <h3 className='text-element'>h1</h3>
             <h1>
               Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.
             </h1>
           </div>
 
-          <div className='component'>
-            <h3>h2</h3>
+          <div className='element component'>
+            <h3 className='text-element'>h2</h3>
             <h2>
               Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.
             </h2>
           </div>
 
-          <div className='component'>
-            <h3>h3</h3>
+          <div className='element component'>
+            <h3 className='text-element'>h3</h3>
             <h3>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h3>
           </div>
 
-          <div className='component'>
-            <h3>h4</h3>
+          <div className='element component'>
+            <h3 className='text-element'>h4</h3>
             <h4>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h4>
           </div>
 
-          <div className='component'>
-            <h3>h5</h3>
+          <div className='element component'>
+            <h3 className='text-element'>h5</h3>
             <h5>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h5>
           </div>
 
-          <div className='component'>
-            <h3>h6</h3>
+          <div className='element component'>
+            <h3 className='text-element'>h6</h3>
             <h6>Lorem ipsum dolor sit amet curabitur blandit tempus porttitor.</h6>
           </div>
 
-          <div className='component'>
-            <h3>p</h3>
+          <div className='element component'>
+            <h3 className='text-element'>p</h3>
             <p>Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum.</p>
           </div>
 
           <div className='component'>
-            <h3>code</h3>
+            <h3 className='text-element'>code</h3>
             <pre>
               <code>{code}</code>
             </pre>
           </div>
 
+        </div>
+      </div>
+
+      <div className='section buttons'>
+        <h2>Buttons</h2>
+        <div className='component' style={{ 'background-color': '#f0f0f0' }}>
+          <button type='button' className='button'>Default</button>&nbsp;
+          <button type='button' className='button btn-alt'>Alternate</button>&nbsp;
+          <button type='button' className='button btn-outline'>Outline</button>&nbsp;
+          <button type='button' className='button btn-primary'>Primary</button>&nbsp;
+          <button type='button' className='button btn-primary btn-outline'>Primary Outline</button>&nbsp;
+          <button type='button' className='button btn-white'>White</button>&nbsp;
+          <button type='button' className='button btn-white btn-outline'>White Outline</button>&nbsp;
+          <button type='button' className='button btn-alert'>Danger</button>
+        </div>
+      </div>
+
+      <div className='section system'>
+        <h2>System Message</h2>
+        <div className='component'>
+
+          <div className='system-message'>
+            <b>Default</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
+
+          <div className='info system-message'>
+            <b>Info</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
+
+          <div className='success system-message'>
+            <b>Success</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
+
+          <div className='warning system-message'>
+            <b>Warning</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
+
+          <div className='error system-message'>
+            <b>Error</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
+
+          <div className='text-success system-message'>
+            <b>Success text</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
+
+          <div className='text-warning system-message'>
+            <b>Warning text</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
+
+          <div className='text-error system-message'>
+            <b>Error text</b> – <a href='/'>Etiam porta</a> sem malesuada magna mollis euismod.
+          </div>
         </div>
       </div>
     </div>
